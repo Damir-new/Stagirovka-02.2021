c3668a34f62cb50b3783fab6dc5dc93686264058
Damir-new <79794352+Damir-new@users.noreply.github.com>
2021-02-28 16:25:42 +0300

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys # импорт ключей,для ввода по клавише Enter
+from par import mail_login
+from par import mail_password
+import time
+
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\Дамир\PycharmProject\sel_pyt\chromedriver\chromedriver.exe")
+
+# говорим WebDriver искать каждый элемент в течение 5 секунд
+driver.implicitly_wait(5)
+
+driver.get("http://gmail.com/")
+
+email_input = driver.find_element_by_id("identifierId") # находит поле ввода на сайте по id этого поля(можно не по id)
+email_input.clear() # очистка поля перед вводом логина
+email_input.send_keys(mail_login) # ввод логина
+email_input.send_keys(Keys.ENTER)
+
+
+
+password_input = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input") # находит поле ввода на сайте по id этого поля(можно не по id)
+password_input.clear() # очистка поля перед вводом пароля
+password_input.send_keys(mail_password) # ввод пароля из файла
+password_input.send_keys(Keys.ENTER)
+
+
+
+
+#butt_input=driver.find_element_by_class_name('gb_uf').click() # нажатие на кнопку в поиске
+#time.sleep(15)
+#poisk_input= driver.find_element_by_xpath('/html/body/div[38]/div/div[2]/div[3]/span[2]') # поле поиска темы
+#poisk_input.clear()
+#time.sleep(3)
+#poisk_input.send_keys('Simbirsoft theme') # поиск темы письма
+#time.sleep(7)
+#poisk_input.send_keys(Keys.ENTER)
+#time.sleep(10)
+
+
+poisk_input = driver.find_element_by_class_name('gb_ff') # ввод в поиск темы письма!
+poisk_input.clear()
+poisk_input.send_keys('subject:Simbirsoft theme')
+poisk_input.send_keys(Keys.ENTER)
+driver.quit()
+
